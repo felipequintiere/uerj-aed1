17a754d99a0c51f7d8cb321a8947ff7cc5ccb471
Felipe Quintiere <felipequintiere@gmail.com>
2026-04-07 20:05:16 -0300

    update

---
+++ 02variaveis/2f.c
@@ -0,0 +1,6 @@
+/*
+f. Modificar o programa anterior de modo que além de n, seja pedido um valor k
+seguido de k inteiros que consistem da lista dos múltiplos que devem ser
+considerados. Por exemplo, se n=17, k=3 seguido dos números 2,3,5, então o
+somatório deve considerar apenas os números 2,3,4,5,6,8,9,10,12,14,15,16.
+*/
